fb4562d7340ba48450f9c80a03ac3c49707db5c0
Sérgio Correia <sergiosdcorreia@gmail.com>
2022-07-12 23:10:22 +0100

    Adding MainNav component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <template>
-  <h1>Hello Vue</h1>
+  <MainNav />
+  <h1 class="mt-6 text-4xl text-center">Sergio Correia</h1>
 </template>
 
 <script>
+import MainNav from "@/components/MainNav.vue";
 export default {
   name: "App",
+  components: { MainNav },
 };
 </script>
