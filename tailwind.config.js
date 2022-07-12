36e6d33d8e8f43b0fc1b90cb5bc0bc2240313cd4
Sérgio Correia <sergiosdcorreia@gmail.com>
2022-07-12 23:34:00 +0100

    Adding project colors to Tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,14 @@ module.exports = {
       fontFamily: {
         sans: ["Open Sans", ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        "brand-orange-1": "#FF824A",
+        "brand-orange-2": "#DC573D",
+        "brand-white": "#E0D9DA",
+        "brand-light-grey": "#AAAAAA",
+        "brand-dark-grey": "#282B30",
+        "brand-black": "#191A1C",
+      },
     },
   },
   plugins: [],
