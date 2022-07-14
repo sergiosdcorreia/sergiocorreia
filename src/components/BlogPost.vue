dee75a853992339431ebd18fee8879e352016cec
Sérgio Correia <sergiosdcorreia@gmail.com>
2022-07-14 11:02:10 +0100

    Adding BlogPost component

---
+++ src/components/BlogPost.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>{{ post.title }}</h1>
+    <p>{{ post.description }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BlogPost",
+  props: {
+    post: {
+      type: String,
+      default: "",
+    },
+  },
+};
+</script>
--- src/views/BlogView.vue
+++ src/views/BlogView.vue
@@ -1,9 +1,29 @@
 <template>
   <h1>This is the Blog page</h1>
+  <BlogPost :post="post" v-for="(post, index) in blogPosts" :key="index" />
 </template>
 
 <script>
+import BlogPost from "@/components/BlogPost.vue";
+
 export default {
   name: "BlogView",
+  components: { BlogPost },
+  data() {
+    return {
+      blogPosts: [
+        {
+          title: "This is a Title!",
+          description: "This is a blog post description",
+          photo: "picture-1",
+        },
+        {
+          title: "This is a Title!",
+          description: "This is a blog post description",
+          photo: "picture-2",
+        },
+      ],
+    };
+  },
 };
 </script>
