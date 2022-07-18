fb0f89292a89b1d6e9fe27580566caa320c493d4
Sérgio Correia <sergiosdcorreia@gmail.com>
2022-07-18 16:21:21 +0100

    Adding tests for the store

---
+++ tests/unit/store/index.test.js
@@ -0,0 +1,20 @@
+import { state, mutations } from "@/store";
+
+describe("state", () => {
+  it("keeps track wether user is logged in", () => {
+    const startingState = state();
+    expect(startingState.isLoggedIn).toBe(false);
+  });
+});
+
+describe("mutations", () => {
+  describe("LOGIN_USER", () => {
+    it("logs the user in", () => {
+      const state = {
+        isLoggedIn: false,
+      };
+      mutations.LOGIN_USER(state);
+      expect(state).toEqual({ isLoggedIn: true });
+    });
+  });
+});
