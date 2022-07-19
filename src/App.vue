b45d8d5aaea4aa94e1a6f2aa3dfc71752b3175a9
Sérgio Correia <sergiosdcorreia@gmail.com>
2022-07-19 23:30:31 +0100

    Hide navigation from Login, Register and ForgotPassword components

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <MainNav />
+  <MainNav v-if="hasNavigation" />
   <router-view />
 </template>
 
@@ -8,5 +8,31 @@ import MainNav from "@/components/MainNav.vue";
 export default {
   name: "App",
   components: { MainNav },
+  data() {
+    return {
+      hasNavigation: null,
+    };
+  },
+  watch: {
+    $route() {
+      this.checkRoute();
+    },
+  },
+  mounted() {
+    this.checkRoute();
+  },
+  methods: {
+    checkRoute() {
+      if (
+        this.$route.name === "login" ||
+        this.$route.name === "register" ||
+        this.$route.name === "forgotpassword"
+      ) {
+        this.hasNavigation = false;
+        return;
+      }
+      this.hasNavigation = true;
+    },
+  },
 };
 </script>
