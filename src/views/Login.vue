252d067d0a693b22c1e2a24d647e600d666dcd4c
Sérgio Correia <sergiosdcorreia@gmail.com>
2022-07-22 18:03:15 +0100

    Login page inputs and buttons

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,9 +1,35 @@
 <template>
-  <div>Login Page</div>
+  <div class="form-wrap">
+    <form class="login">
+      <p>
+        Don't have an account?
+        <router-link :to="{ name: 'register' }">Register</router-link>
+      </p>
+      <h2>Login</h2>
+      <div class="inputs">
+        <div class="input">
+          <input v-model="email" type="email" placeholder="Email" />
+        </div>
+        <div class="input">
+          <input v-model="password" type="password" placeholder="Password" />
+        </div>
+      </div>
+      <router-link :to="{ name: 'forgotpassword' }"
+        >Forgot your password?</router-link
+      >
+      <button>Sign In</button>
+    </form>
+  </div>
 </template>
 
 <script>
 export default {
   name: "LogIn",
+  data() {
+    return {
+      email: null,
+      password: null,
+    };
+  },
 };
 </script>
