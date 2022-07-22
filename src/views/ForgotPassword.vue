5831aed1384db14082d788df4239177a09262e7d
Sérgio Correia <sergiosdcorreia@gmail.com>
2022-07-22 22:11:33 +0100

    ForgotPassword component inputs and buttons

--- src/views/ForgotPassword.vue
+++ src/views/ForgotPassword.vue
@@ -1,9 +1,25 @@
 <template>
-  <div>Forgot Password Page</div>
+  <div class="form-wrap">
+    <form class="reset">
+      <h2>Reset Password</h2>
+      <p>Forgot your password? Enter your email to reset it</p>
+      <div class="inputs">
+        <div class="input">
+          <input v-model="email" type="email" placeholder="Email" />
+        </div>
+      </div>
+      <button>Reset</button>
+    </form>
+  </div>
 </template>
 
 <script>
 export default {
   name: "ForgotPassword",
+  data() {
+    return {
+      email: null,
+    };
+  },
 };
 </script>
