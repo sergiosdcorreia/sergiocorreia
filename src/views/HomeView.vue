b1460f84bf0c380c9f61698e39c0a6a046eba6b9
Sérgio Correia <sergiosdcorreia@gmail.com>
2022-08-05 16:06:13 +0100

    Adding content for the home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,28 @@
 <template>
   <div class="home">
-    <h1>"Welcome to Your Home Page"</h1>
+    <h1>Sergio Correia</h1>
+    <h2>Building a better web.</h2>
+    <p>Front End Web Developer</p>
+    <h2>Technologies</h2>
+    <h3>Development</h3>
+    <h4>HTML/CSS</h4>
+    <p>Bootstrap</p>
+    <p>Tailwind</p>
+    <h4>JavaScript</h4>
+    <p>Vue</p>
+    <p>React</p>
+    <h4>Testing</h4>
+    <p>Jest</p>
+    <h4>Source Control</h4>
+    <p>GitHub</p>
+    <p>GitLab</p>
+    <h4>Backend</h4>
+    <p>Firebase</p>
+    <h3>Design</h3>
+    <p>Photoshop</p>
+    <p>Illustrator</p>
+    <p>Adobe XD</p>
+    <p>Figma</p>
   </div>
 </template>
 
