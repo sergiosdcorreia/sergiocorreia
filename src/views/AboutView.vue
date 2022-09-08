a7eabd3bf7b76f27ebb298b79823e0127d6aef9d
Sérgio Correia <sergiosdcorreia@gmail.com>
2022-09-08 04:15:26 +0100

    About page styling

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,3 +1,13 @@
 <template>
-  <h1 class="text-6xl font-bold">This is an about page</h1>
+  <h1
+    class="title text-center pt-10 text-2xl md:text-6xl font-bold select-none"
+  >
+    about
+  </h1>
 </template>
+<style lang="scss" scoped>
+.title {
+  font-family: "Baloo 2", sans-serif;
+  color: var(--light-grey);
+}
+</style>
